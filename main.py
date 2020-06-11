fe113be26738324cd24f1d4129efa40d648bb152
Goodenberg <63363479+Goodenberg@users.noreply.github.com>
2020-06-11 12:29:01 +0200

    Explore imap protocol via interactive shell

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-from imaplib import IMAP4
+from imaplib import IMAP4_SSL
 import email
+import getpass
 
-# IMAP PROTOCOL DEFINITION: https://tools.ietf.org/html/rfc3501#section-6.4.7
+# IMAP PROTOCOL SPECIFICATION: https://tools.ietf.org/html/rfc3501
+# imaplib documentation: https://docs.python.org/3.8/library/imaplib.html
+
+# delete an email:
+# imap_client.store('email_id', '+FLAGS', '\\DELETED')
+
+
+host = input('Enter imap host: ')
+
+imap_client = IMAP4_SSL(host)
+
+imap_client.noop()
+
+user = input("Enter imap user: ")
+pw = getpass.getpass(prompt='Enter imap password:')
+imap_client.login(user, pw)
+
+imap_client.list()
+
+from pprint import pprint
+
+pprint(imap_client.list())
+
+imap_client.select(mailbox="Spam", readonly=False)
+
+imap_client.search(None, 'all')
+
+imap_client.search(None, '1')
